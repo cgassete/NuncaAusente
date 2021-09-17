19c3575191d9a716ab859ba8a8008b94e8680e7f
Carmelo Ambrosio Gassette <ambrosc@emea.corpdir.net>
2021-09-17 14:55:07 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace NuncaAusente
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbError = new System.Windows.Forms.Label();
+            this.tbMinutos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(176, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descansa un poco, te lo has ganado";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(511, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Descansar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbError
+            // 
+            this.lbError.AutoSize = true;
+            this.lbError.ForeColor = System.Drawing.Color.Red;
+            this.lbError.Location = new System.Drawing.Point(123, 168);
+            this.lbError.Name = "lbError";
+            this.lbError.Size = new System.Drawing.Size(0, 15);
+            this.lbError.TabIndex = 2;
+            // 
+            // tbMinutos
+            // 
+            this.tbMinutos.Location = new System.Drawing.Point(378, 89);
+            this.tbMinutos.Name = "tbMinutos";
+            this.tbMinutos.Size = new System.Drawing.Size(100, 23);
+            this.tbMinutos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "¿Cuántos minutos desea activar la opción?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Desarrollado por Carmelo Gassette (Devnyx)";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(614, 394);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(129, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Apoyar al desarrollador";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(67, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "FAQ\'s";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(303, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "¿Cómo cerrar la app antes de acabar el tiempo definido?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(427, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Se debe ir al administrador de tareas y cerrar el proceso llamado \"DevnyxTeams\"";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(374, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "¿Luego de seleccionar el tiempo y hacer click en el Botón desaparece?";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(737, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "La app se hace invisible para que al momento de presentar la pantalla no aparezca" +
+    " en la barra de herramienta y pueda generar sospechas  ;)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(486, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "El Teams ya no me aparece como inactivo, pero se me bloquea aún la sesión de Wind" +
+    "ows...";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(40, 297);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(710, 15);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Para que no se bloquee el Windows recomendamos que si tiene 2 pantallas deje en u" +
+    "na siempre reproduciendo un video de youtube, ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(40, 316);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(706, 15);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "así sea sin volumen. Si cuenta con una sola pantalla, al momento de alejarse del " +
+    "ordenador, debe dejar un video reproduciendo en la ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(40, 335);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(235, 15);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "pantalla principal y esta herramienta activa.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMinutos);
+            this.Controls.Add(this.lbError);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbError;
+        private System.Windows.Forms.TextBox tbMinutos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NuncaAusente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex) { lbError.Text = ex.Message; }
+        }
+        private void MoveCursor()
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X + 252, Cursor.Position.Y + 152);
+            Cursor.Position = new Point(Cursor.Position.X - 250, Cursor.Position.Y - 150);
+            //Cursor.Clip = new Rectangle(this.Location, this.Size);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+                this.Visible = false;
+                // Set the Current cursor, move the cursor's Position,
+                // and set its clipping rectangle to the form. 
+                Int32 Repeticiones = Convert.ToInt32(tbMinutos.Text);
+                Int32 Contador = 1;
+                while (Contador <= Repeticiones)
+                {
+                    MoveCursor();
+                    Contador += 1;
+                    Thread.Sleep(60000);
+                }
+                this.Enabled = true;
+                this.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                lbError.Text = ex.Message;
+                this.Enabled = true;
+                this.Visible = true;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "https://www.paypal.me/carmelogassette";
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                lbError.Text = ex.Message;
+                this.Enabled = true;
+                this.Visible = true;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
